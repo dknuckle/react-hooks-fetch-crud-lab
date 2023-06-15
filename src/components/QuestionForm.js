809d63100ffda33cc2ebcc939420196057b85020
Dalton Knuckles <djck244@gmail.com>
2023-06-15 11:33:46 -0700

    complete lab

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-function QuestionForm(props) {
+function QuestionForm({ onAddQuestion }) {
   const [formData, setFormData] = useState({
     prompt: "",
     answer1: "",
@@ -9,6 +9,9 @@ function QuestionForm(props) {
     answer4: "",
     correctIndex: 0,
   });
+  const [isMounted, setIsMounted] = useState(true);
+  const [currentId, setCurrentId] = useState(1); // Counter for generating IDs
+
 
   function handleChange(event) {
     setFormData({
@@ -19,8 +22,47 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+    const newQuestion = {
+      id: currentId, // Include the ID
+      prompt: formData.prompt,
+      answers: [
+        formData.answer1,
+        formData.answer2,
+        formData.answer3,
+        formData.answer4,
+      ],
+      correctIndex: parseInt(formData.correctIndex),
+    };
+
+    fetch("http://localhost:4000/questions", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newQuestion),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (isMounted) {
+        onAddQuestion(data);
+        setFormData({
+          prompt: "",
+          answer1: "",
+          answer2: "",
+          answer3: "",
+          answer4: "",
+          correctIndex: 0,
+        });
+      }
+        setCurrentId(currentId + 1); // Increment the ID counter
+      })
+      .catch((error) => console.log(error));
   }
+
+  useEffect(() => {
+    return () => {
+      setIsMounted(false);
+      // Cleanup function to cancel any ongoing requests
+    };
+  }, []);
 
   return (
     <section>
@@ -78,10 +120,10 @@ function QuestionForm(props) {
             value={formData.correctIndex}
             onChange={handleChange}
           >
-            <option value="0">{formData.answer1}</option>
-            <option value="1">{formData.answer2}</option>
-            <option value="2">{formData.answer3}</option>
-            <option value="3">{formData.answer4}</option>
+            <option value="0">Answer 1</option>
+            <option value="1">Answer 2</option>
+            <option value="2">Answer 3</option>
+            <option value="3">Answer 4</option>
           </select>
         </label>
         <button type="submit">Add Question</button>
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,66 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetchQuestions();
+  }, []);
+
+  const fetchQuestions = () => {
+    fetch("http://localhost:4000/questions")
+      .then((response) => response.json())
+      .then((data) => setQuestions(data))
+      .catch((error) => console.log(error));
+  };
+
+  const deleteQuestion = (id) => {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then(() => {
+        const updatedQuestions = questions.filter(
+          (question) => question.id !== id
+        );
+        setQuestions(updatedQuestions);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const updateCorrectIndex = (id, correctIndex) => {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ correctIndex }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const updatedQuestions = questions.map((question) => {
+          if (question.id === id) {
+            return { ...question, correctIndex };
+          }
+          return question;
+        });
+        setQuestions(updatedQuestions);
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questions.map((question, index) => (
+          <QuestionItem
+            key={`${question.id}-${index}`} // Generate a unique key
+            question={question}
+            onDelete={deleteQuestion}
+            onUpdateCorrectIndex={updateCorrectIndex}
+            questionNumber={index}
+          />
+        ))}
+      </ul>
     </section>
   );
 }
